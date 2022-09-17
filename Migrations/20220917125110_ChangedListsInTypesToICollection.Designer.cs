2cf4fbe0f68ceaaf4e570b76353d0429c66a6219
UchiTesing <uchitesting57@gmail.com>
2022-09-17 14:52:59 +0200

    Add migration to apply changes to types

--- Migrations/20220917125110_ChangedListsInTypesToICollection.Designer.cs
+++ Migrations/20220917125110_ChangedListsInTypesToICollection.Designer.cs
@@ -4,6 +4,7 @@ using EF_MM.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF_MM.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220917125110_ChangedListsInTypesToICollection")]
+    partial class ChangedListsInTypesToICollection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
