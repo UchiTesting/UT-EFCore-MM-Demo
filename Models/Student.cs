66467a09bf2a25eed06ed5f4903eea144d1ac043
UchiTesing <uchitesting57@gmail.com>
2022-09-17 12:33:52 +0200

    Add Student type

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace EF_MM.Models
+{
+    public class Student
+    {
+        public Student() { }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
