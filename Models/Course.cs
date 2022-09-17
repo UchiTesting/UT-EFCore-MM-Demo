faa9f186edee4e0cf5338f95e08018c5a6e7c0b0
UchiTesing <uchitesting57@gmail.com>
2022-09-17 14:28:14 +0200

    Updated types with annotations

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,10 +1,17 @@
-﻿namespace EF_MM.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_MM.Models
 {
     public class Course
     {
         public Course() { }
 
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string Title { get; set; }
+
+        // Navigation property on a collection
+        IEnumerable<Student> Students { get; set; }
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,18 @@
-﻿namespace EF_MM.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_MM.Models
 {
     public class Student
     {
         public Student() { }
 
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+
+        // Navigation property on a collection
+        IEnumerable<Course> Courses { get; set; }
     }
 }
