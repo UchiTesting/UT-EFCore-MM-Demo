332a509c98aee66d0acb5b0a042624eaf9f38035
UchiTesing <uchitesting57@gmail.com>
2022-09-17 14:27:52 +0200

    Add EF Core NuGet packages and setup DbContext

---
+++ Persistence/MyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EF_MM.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_MM.Persistence
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            string connectionString =
+                "Server=(localdb)\\mssqllocaldb;Database=ManyToManyDemo;Trusted_Connection=True;MultipleActiveResultSets=true";
+            if (optionsBuilder is not null)
+                optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Course>()
+                .Property(p => p.Id)
+                .HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<Student>()
+                .Property(p => p.Id)
+                .HasDefaultValueSql("NEWID()");
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
