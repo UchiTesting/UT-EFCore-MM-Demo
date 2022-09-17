c67681415a72b9ba3f41c4f89743108507eb7ae0
UchiTesing <uchitesting57@gmail.com>
2022-09-17 18:57:18 +0200

    Add Main code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EF_MM.Demos;
+
+EfDemos efDemos = new EfDemos();
+
+/// CRUD operations on MMR (Many-to-Many Relationship)
+/// C: AddDotNetCoursesToAnnie()
+/// R: DisplayAnniesCourses()
+/// U: UpdatePmCourseTo201() in particular
+/// D: AddHibernateThenRemoveIt() The 1st part adds Hibernate course and commit to DB. The 2nd part reverts and commit.
+/// 
+/// Feel free to modify and play around with this code.
+
+efDemos.DisplayAnniesCourses();
+
+// Execute those methods
+// - sequentially (i.e. one after the other in order)
+// - and exclusively (i.e. one at a time)
+#region Actual Demos
+efDemos.AddDotNetCoursesToAnnie();
+//efDemos.UpdatePmCourseTo201();
+//efDemos.AddHibernateThenRemoveIt();
+//efDemos.CreateDevOpsCourseThenAddToAnnieThenDeleteDevOpsCourse();
+#endregion
+efDemos.DisplayAnniesCourses();
